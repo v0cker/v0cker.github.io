cc675321ac9ef5680dc8873aca4fac7a1fb7ed72
v0cker <coonyash@gmail.com>
2018-02-14 00:07:54 +0200

    alpha 2.0

--- tictac.js
+++ tictac.js
@@ -138,3 +138,14 @@ function minimax(newBoard, player) {
 
 	return moves[bestMove];
 }
+$(".ripple").on("click",function(event){
+$(this).append("<span class='ripple-effect'>");
+$(this).find(".ripple-effect").css({
+   left:event.pageX-$(this).position().left,
+    top:event.pageY-$(this).position().top
+  }).animate({
+    opacity: 0,
+  }, 1500, function() {
+   $(this).remove();
+  });
+});
