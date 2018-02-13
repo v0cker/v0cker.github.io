db30355583787e2cd9194a88936a77a6ad2dad8b
v0cker <coonyash@gmail.com>
2018-02-13 23:38:12 +0200

    1.25

--- tictac.js
+++ tictac.js
@@ -9,12 +9,14 @@ const winCombos = [
 	[1, 4, 7],
 	[2, 5, 8],
 	[0, 4, 8],
-	[2, 4, 6]
+	[6, 4, 2]
 ]
+
 const cells = document.querySelectorAll('.cell');
 startGame();
+
 function startGame() {
-	document.querySelector(".endgame").style.display="none";
+	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].innerText = '';
@@ -22,65 +24,74 @@ function startGame() {
 		cells[i].addEventListener('click', turnClick, false);
 	}
 }
+
 function turnClick(square) {
 	if (typeof origBoard[square.target.id] == 'number') {
-	turn(square.target.id, huPlayer);
-	if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
-
+		turn(square.target.id, huPlayer)
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
 	}
 }
+
 function turn(squareId, player) {
 	origBoard[squareId] = player;
 	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player);
+	let gameWon = checkWin(origBoard, player)
 	if (gameWon) gameOver(gameWon)
 }
+
 function checkWin(board, player) {
-	let plays = board. reduce((a, e, i) => 
+	let plays = board.reduce((a, e, i) =>
 		(e === player) ? a.concat(i) : a, []);
 	let gameWon = null;
 	for (let [index, win] of winCombos.entries()) {
 		if (win.every(elem => plays.indexOf(elem) > -1)) {
 			gameWon = {index: index, player: player};
 			break;
-		}	
+		}
 	}
 	return gameWon;
 }
+
 function gameOver(gameWon) {
 	for (let index of winCombos[gameWon.index]) {
 		document.getElementById(index).style.backgroundColor =
-		gameWon.player == huPlayer ? "blue" : "red"
+			gameWon.player == huPlayer ? "blue" : "red";
 	}
-	for (let i = 0; i < cells.length; i++) {
-		cells[i].removeEventListener('click', turnClick, false)
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
 	}
 	declareWinner(gameWon.player == huPlayer ? "Ты выиграл!" : "Ты проиграл.");
 }
+
 function declareWinner(who) {
 	document.querySelector(".endgame").style.display = "block";
 	document.querySelector(".endgame .text").innerText = who;
 }
+
 function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number')
+	return origBoard.filter(s => typeof s == 'number');
 }
+
 function bestSpot() {
 	return minimax(origBoard, aiPlayer).index;
 }
+
 function checkTie() {
 	if (emptySquares().length == 0) {
-		for(let i = 0; i < cells.length; i++) {
+		for (var i = 0; i < cells.length; i++) {
 			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', turnClick, false);
 		}
-		declareWinner("Ничья.");
+		declareWinner("Ничья!")
 		return true;
 	}
 	return false;
 }
+
 function minimax(newBoard, player) {
 	var availSpots = emptySquares(newBoard);
-	if (checkWin(newBoard, player)) {
+
+	if (checkWin(newBoard, huPlayer)) {
 		return {score: -10};
 	} else if (checkWin(newBoard, aiPlayer)) {
 		return {score: 10};
@@ -88,7 +99,7 @@ function minimax(newBoard, player) {
 		return {score: 0};
 	}
 	var moves = [];
-	for (let i = 0; i < availSpots.length; i++) {
+	for (var i = 0; i < availSpots.length; i++) {
 		var move = {};
 		move.index = newBoard[availSpots[i]];
 		newBoard[availSpots[i]] = player;
@@ -100,13 +111,16 @@ function minimax(newBoard, player) {
 			var result = minimax(newBoard, aiPlayer);
 			move.score = result.score;
 		}
+
 		newBoard[availSpots[i]] = move.index;
+
 		moves.push(move);
 	}
+
 	var bestMove;
 	if(player === aiPlayer) {
 		var bestScore = -10000;
-		for (let i = 0; i < moves.length; i++) {
+		for(var i = 0; i < moves.length; i++) {
 			if (moves[i].score > bestScore) {
 				bestScore = moves[i].score;
 				bestMove = i;
@@ -114,12 +128,13 @@ function minimax(newBoard, player) {
 		}
 	} else {
 		var bestScore = 10000;
-		for (let i = 0; i < moves.length; i++) {
+		for(var i = 0; i < moves.length; i++) {
 			if (moves[i].score < bestScore) {
 				bestScore = moves[i].score;
 				bestMove = i;
 			}
 		}
 	}
+
 	return moves[bestMove];
 }
